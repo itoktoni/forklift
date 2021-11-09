9c88da74a58fda3d7ab1a1302edc7cec559bf80d
itoktoni <itok.toni@gmail.com>
2021-11-09 11:04:20 +0700

    update forklift

--- resources/views/frontend/trucking/meta.blade.php
+++ resources/views/frontend/trucking/meta.blade.php
@@ -1,6 +1,7 @@
 <title>Trucking - Transportation and Logistics Website Template Home 04</title>
-<meta name="description" content="Trucking is transportation and Logistics website template">
-<meta name="author" content="pixel-industry">
-<meta name="keywords" content="transportation, logistics, transportation template, logistics template, cargo, business">
+<meta name="description" content="Nusa Engineering serves service and maintenance of various types of forklifts, support with professional and
+experienced engineers.">
+<meta name="author" content="admin">
+<meta name="keywords" content="forklift, sparepart, service, forklift jakarta, service forklift">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
