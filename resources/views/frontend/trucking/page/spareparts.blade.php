79dc0c181d09d0674691fdf0d67104dd2d487f37
itoktoni <itok.toni@gmail.com>
2021-11-09 10:58:58 +0700

    update image

--- resources/views/frontend/trucking/page/spareparts.blade.php
+++ resources/views/frontend/trucking/page/spareparts.blade.php
@@ -139,7 +139,7 @@
             <div class="col-md-9">
                 <div class="row">
                     <div class="col-md-5">
-                        <img class="mt-1" src="{{ Helper::files('spareparts/spareparts.jpg') }}" alt="" />
+                        <img class="mt-1" src="{{ Helper::frontend('img/pics/spareparts.jpg') }}" alt="" />
                     </div><!-- .col-md-4 end -->
 
                     <div class="col-md-7">
